2eaacf7f2e52fdc01e0a1ec742128696fdf70d2b
NoCtrlZ <phantomofrotten@gmail.com>
2022-06-03 14:43:54 +0900

    add bench

--- halo2_proofs/benches/arithmetic.rs
+++ halo2_proofs/benches/arithmetic.rs
@@ -1,35 +1,34 @@
 #[macro_use]
 extern crate criterion;
 
-use crate::arithmetic::small_multiexp;
+use crate::arithmetic::best_multiexp;
 use crate::pasta::{EqAffine, Fp};
 use crate::poly::commitment::Params;
 use group::ff::Field;
 use halo2_proofs::*;
 
-use criterion::{black_box, Criterion};
+use criterion::{black_box, BenchmarkId, Criterion};
 use rand_core::OsRng;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let rng = OsRng;
-
-    // small multiexp
-    {
-        let params: Params<EqAffine> = Params::new(5);
-        let g = &mut params.get_g();
-        let len = g.len() / 2;
-        let (g_lo, g_hi) = g.split_at_mut(len);
-
-        let coeff_1 = Fp::random(rng);
-        let coeff_2 = Fp::random(rng);
-
-        c.bench_function("double-and-add", |b| {
-            b.iter(|| {
-                for (g_lo, g_hi) in g_lo.iter().zip(g_hi.iter()) {
-                    small_multiexp(&[black_box(coeff_1), black_box(coeff_2)], &[*g_lo, *g_hi]);
-                }
+    let mut group = c.benchmark_group("arithmetic-small_multiexp");
+    for k in 5..16 {
+        group
+            .bench_function(BenchmarkId::new("k", k), |b| {
+                let rng = OsRng;
+                let params: Params<EqAffine> = Params::new(k);
+                let g = &mut params.get_g();
+                let len = g.len() / 2;
+                let (g_lo, g_hi) = g.split_at_mut(len);
+                let coeff_1 = Fp::random(rng);
+                let coeff_2 = Fp::random(rng);
+                b.iter(|| {
+                    for (g_lo, g_hi) in g_lo.iter().zip(g_hi.iter()) {
+                        best_multiexp(&[black_box(coeff_1), black_box(coeff_2)], &[*g_lo, *g_hi]);
+                    }
+                })
             })
-        });
+            .sample_size(30);
     }
 }
 
